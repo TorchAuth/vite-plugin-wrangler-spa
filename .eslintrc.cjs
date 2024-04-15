35d6ee031b26751daa13c4cd30e50857a92b9df4
Bruce Harrison <leeharrison1984@gmail.com>
2024-04-15 14:56:15 -0500

    wip

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+};
