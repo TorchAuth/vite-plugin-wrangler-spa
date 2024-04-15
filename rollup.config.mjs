1dcd8ad5da3d1a9db9bbbaad69f62cdf8decb730
Bruce Harrison <leeharrison1984@gmail.com>
2024-04-15 16:01:19 -0500

    wip

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,22 @@
-import { defineConfig } from "rollup";
-import copy from "rollup-plugin-copy";
-import ts from "rollup-plugin-ts";
+import { defineConfig } from 'rollup';
+import copy from 'rollup-plugin-copy';
+import ts from 'rollup-plugin-ts';
 
 const options = defineConfig({
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: {
-    dir: "dist",
-    format: "esm",
+    dir: 'dist',
+    format: 'es',
   },
-  external: ["wrangler", "vite", "fs", "node:stream", "set-cookie-parser"],
+  external: ['wrangler', 'vite', 'fs', 'node:stream', 'set-cookie-parser'],
   plugins: [
     copy({
       targets: [
-        { src: "./README.md", dest: "./dist" },
-        { src: "./LICENSE", dest: "./dist" },
+        { src: './README.md', dest: './dist' },
+        { src: './LICENSE', dest: './dist' },
       ],
     }),
-    ts({
-      transpiler: "swc",
-    }),
+    ts(),
   ],
 });
 
